34b81082bca06b44f07fd0b4030b80558d1a56b7
Sphinxzzzz <112004827+Sphinxzzzz@users.noreply.github.com>
2022-11-24 12:29:32 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
+global.owner = ['27634090203',''] //owner number +916398118678
 global.wame = ['27634090203'] 
 global.pemilik = ['27634090203'] //owner number +27634090203
 global.premium = ['27634090203'] //premium number +27634090203
 global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.footer = 'Thakur'
+global.botnma = 'Thakur Sahab' //bot name
+global.botname = 'Thakur Sahab'
+global.ownernma = 'Thakur Sahab' //owner name
+global.packname = 'Thakur Sahab' //sticker package name
+global.author = 'By Mrityu' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
